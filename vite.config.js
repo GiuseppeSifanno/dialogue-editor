6f6030f0fc5dc486a20714113b1738d93ec46b6f
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-08-07 15:03:30 +0200

    aggiornato percorso di deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/dialogue-editor/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/dialogue-editor/' : '/',
   plugins: [react()]
-})
+}))
