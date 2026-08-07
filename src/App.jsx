810d95b27beae084762a5cc77e548dea3d18a7f8
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-08-07 15:55:38 +0200

    Risolto problema con import di librerie (cambiato da next a react)

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from "react"
 import HomeScreen   from "./screens/HomeScreen"
 import EditorScreen from "./screens/EditorScreen"
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
-
 function App() {
   const [currentView, setCurrentView] = useState('home')
   const [attoId, setAttoId]           = useState(null)
@@ -24,8 +21,6 @@ function App() {
   }
 
   return (
-    <SpeedInsights /> && 
-    <Analytics /> && 
     currentView === 'home'
     ? <HomeScreen onOpen={handleOpenAtto} />
     : <EditorScreen attoId={attoId} onBack={handleBack} onRename={handleRename} />
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights /> 
+    <Analytics />
   </StrictMode>,
 )
