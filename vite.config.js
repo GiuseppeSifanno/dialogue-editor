bcf4e241e1b5c2d407e090c7e239c1a0f2f3d37e
GiuseppeSifanno <90522347+GiuseppeSifanno@users.noreply.github.com>
2026-08-07 15:27:03 +0200

    Aggiornamenti

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'github' ? '/dialogue-editor/' : '/',
+  base: '/dialogue-editor/',
   plugins: [react()]
 }))
